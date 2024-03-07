9a1105db68672d528d65c5fbb1b09cebf51b3a69
6jeez <159715609+6jeez@users.noreply.github.com>
2024-03-07 23:05:01 +0200

    Add files via upload

---
+++ create_session_pyrogram.py
@@ -0,0 +1,15 @@
+from pyrogram import Client
+import asyncio
+
+
+async def get_user_id():
+    api_id = input("Введите API_ID: ")
+    api_hash = input("Введите API_HASH: ")
+    session_name = input("Введите имя сессии: ")
+
+    async with Client(session_name, api_id, api_hash) as client:
+        me = await client.get_me()
+
+        print("Ваш user_id:", me.id)
+
+asyncio.run(get_user_id())

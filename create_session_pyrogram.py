99115ba48c162c1a8fcb84a30d9b2c3fa9b789ba
jeeznotbad <159715609+6jeez@users.noreply.github.com>
2024-07-14 15:33:27 +0300

    Update create_session_pyrogram.py

--- create_session_pyrogram.py
+++ create_session_pyrogram.py
@@ -5,11 +5,13 @@ import asyncio
 async def get_user_id():
     api_id = input("Введите API_ID: ")
     api_hash = input("Введите API_HASH: ")
-    session_name = input("Введите имя сессии: ")
 
-    async with Client(session_name, api_id, api_hash) as client:
-        me = await client.get_me()
+    while True:
+        session_name = input("Введите имя сессии: ")
 
-        print("Ваш user_id:", me.id)
+        async with Client(session_name, api_id, api_hash) as client:
+            me = await client.get_me()
+
+            print("Ваш user_id:", me.id)
 
 asyncio.run(get_user_id())
